c0f1be65d3bf3d056716853b0aa30341ec8de861
hossam fawzy <eng.hossam.fawzy.elsafty@gmail.com>
2018-09-27 13:37:42 +0200

    Initial Commit

---
+++ IslamAcademy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IslamAcademy
+//
+//  Created by Hosam Elsafty on 9/27/18.
+//  Copyright © 2018 Hosam Elsafty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
